5db5b4d13169b7615bce5b6844fdd712c3ebe3f5
Ion Scripnic <asdfree03@gmail.com>
2020-04-25 18:38:30 +0300

    3'rd commit

---
+++ src/main/java/ap/registration/Main.java
@@ -0,0 +1,18 @@
+package ap.registration;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.printf("Welcome");
+    }
+}
