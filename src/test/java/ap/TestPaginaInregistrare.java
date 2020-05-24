e90a248a09140092ffae9692ed23efa61f691ff9
CristiTurcan <kristi.turcan@gmail.com>
2020-05-24 19:16:05 +0300

    AP-26 #comment Am adaugat clasa de testare

---
+++ src/test/java/ap/TestPaginaInregistrare.java
@@ -0,0 +1,15 @@
+package ap;
+import ap.controllers.Inregistrare;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPaginaInregistrare {
+    @Test
+    public void ads()  {
+        Inregistrare tester = new Inregistrare();
+        assertEquals();
+        //assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
+       // assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+    }
+}
